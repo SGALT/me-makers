3ff2191a4f13f4dbec33602202f359941c30318e
Sylvain Galtier <sylvaingaltier@gmail.com>
2018-11-20 15:39:23 +0100

    show of prestation controller with view

--- app/controllers/prestations_controller.rb
+++ app/controllers/prestations_controller.rb
@@ -1,2 +1,12 @@
 class PrestationsController < ApplicationController
+  before_action :set_prestation, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_prestation
+    @prestation = Prestation.find(params[:id])
+  end
 end

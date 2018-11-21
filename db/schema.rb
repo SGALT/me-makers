5ecb50e052296dfd753aaf8e6ba88d7a8a2709b5
remysichel <remysichel@gmail.com>
2018-11-21 12:32:58 +0100

    debug navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_154014) do
+ActiveRecord::Schema.define(version: 2018_11_21_102251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,9 +31,9 @@ ActiveRecord::Schema.define(version: 2018_11_20_154014) do
     t.bigint "project_id"
     t.bigint "user_id"
     t.integer "price"
-    t.string "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "state"
     t.index ["project_id"], name: "index_prestations_on_project_id"
     t.index ["user_id"], name: "index_prestations_on_user_id"
   end

741ec037f606a4dd62ab85748a73cb6ebef766e3
Sylvain Galtier <sylvaingaltier@gmail.com>
2018-11-23 14:16:07 +0100

    modification of the table messages and remove of table reviews (add on prestation)

--- app/models/prestation.rb
+++ app/models/prestation.rb
@@ -1,6 +1,7 @@
 class Prestation < ApplicationRecord
   belongs_to :project
   belongs_to :artisan, class_name: "User"
+  has_many :messages
 
 
   enum state: { pending: 0, booked: 1, declined: 2, done: 3 }
